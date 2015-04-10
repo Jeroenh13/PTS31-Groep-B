1b5356160edf297f0b96ebc1573b92e8929c8ae3
KittyvdP <kittyvdp@gmail.com>
2015-04-10 20:01:49 +0200

    Screen overgangen toegevoegd

---
+++ DontCrash/src/dontcrash/MenuController.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dontcrash;
+
+import java.io.IOException;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Kitty
+ */
+public class MenuController 
+{
+    @FXML Button btnplay;
+    @FXML Parent root;
+    
+    public void Play(Event evnt) throws IOException
+    {
+        Stage stage=(Stage) btnplay.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/fxml/CharacterSelect.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
