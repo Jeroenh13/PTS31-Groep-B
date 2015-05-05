40a680862b60079cc4f529805a03c2c9a3760249
jeroenh13@live.nl <Sluiskamp33!>
2015-05-05 16:03:37 +0200

    character uitgebreid

--- DontCrash/src/dontcrash/Character.java
+++ DontCrash/src/dontcrash/Character.java
@@ -15,6 +15,10 @@ public class Character
     int characterID;
     String name;
     int cost;
+    double speed;
+    float direction;
+    double curX;
+    double curY;
     
     /**
      * Initializes a new character using a player and a characterid
@@ -35,5 +39,33 @@ public class Character
     public void gameOver()
     {
         //gg
+    }
+    
+    /**
+     * Sets the speed for the character
+     * @param speed new speed value
+     */
+    public void setSpeed(double speed)
+    {
+        this.speed = speed;
+    }
+    
+    /**
+     * Sets the direction for the character
+     * @param direction new direction value
+     */
+    public void setDirection(float direction)
+    {
+        
+    }
+    
+    /**
+     * Sets the position of the character
+     * @param X the X position
+     * @param Y thy Y position
+     */
+    public void setPosition(double X, double Y)
+    {
+        
     }
 }
