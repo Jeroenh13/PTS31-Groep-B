e31cb670a07ffb924aa101ddff8a93c2d0e99a8b
SebastiaanvGaans <sebastiaanvgaans@hotmail.com>
2015-06-24 11:26:43 +0200

    runServer

--- DontCrash/src/Sockets/runServer.java
+++ DontCrash/src/Sockets/runServer.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Handles the creation of a new server trough sockets.
  */
 package Sockets;
 
@@ -11,7 +9,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
 import java.net.Socket;
-import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import RMI.Server;
@@ -20,7 +17,7 @@ import RMI.Server;
  *
  * @author Bas
  */
-class runServer implements Runnable {
+class RunServer implements Runnable {
     
     
     Socket so = null;
@@ -30,41 +27,20 @@ class runServer implements Runnable {
     ObjectInputStream in ;
     ObjectOutputStream out;
 
-    public runServer(Socket accept) {
+    public RunServer(Socket accept) {
         so = accept;
     }
 
     @Override
     public void run() {
-        System.out.println("Open");
-        String input = "";
         try {
-            
             outStream = so.getOutputStream();
             inStream = so.getInputStream();
             in = new ObjectInputStream(inStream);
             out = new ObjectOutputStream(outStream);
-            System.out.println("Open");
-            input =(String) in.readObject();
-            System.out.println("done reading");
+            out.writeObject(Server.createNewServer("Chat", "Chat"));
         } catch (IOException ex) {
-            Logger.getLogger(runServer.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(runServer.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        
-        System.out.println("if");
-        if(input.equals("newServer"))
-        {
-            try {
-                
-        System.out.println("new");
-                Server.createNewServer("Admin");
-                
-        System.out.println("done");
-            } catch (IOException ex) {
-                Logger.getLogger(runServer.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            Logger.getLogger(RunServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     
